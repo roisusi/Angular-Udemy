e3b51462e8e5f6f6a79b56b4bbcae3cd01dcdf4f
roisusi <roisusi@gmail.com>
2022-08-22 17:13:11 +0300

    Section 5 - Init
    Angular 11

---
+++ resturant-project/src/app/header/shared/ingredients.model.ts
@@ -0,0 +1,4 @@
+export class Ingredients {
+  //another way to config
+  constructor(public name: string, public amount: number) {}
+}
--- resturant-project/src/app/shopping-list/shoping-list.component.ts
+++ resturant-project/src/app/shopping-list/shoping-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../header/shared/ingredients.model';
 
 @Component({
   selector: 'app-shoping-list',
   templateUrl: './shoping-list.component.html',
-  styleUrls: ['./shoping-list.component.css']
+  styleUrls: ['./shoping-list.component.css'],
 })
 export class ShopingListComponent implements OnInit {
+  ingredients: Ingredients[] = [
+    new Ingredients('Apple', 5),
+    new Ingredients('Tomatoes', 10),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
