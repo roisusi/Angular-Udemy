2d646609ec37ee2476a9f93004f9afbd25f371d0
Roi Susi <roisusi@gmail.com>
2022-11-27 07:56:27 +0200

    more aff assig 4

--- ass-04-Games/src/app/game-control/game-control.component.ts
+++ ass-04-Games/src/app/game-control/game-control.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  styleUrls: ['./game-control.component.css'],
 })
 export class GameControlComponent implements OnInit {
+  globalInterval: NodeJS.Timer;
+  @Output() intervallFired = new EventEmitter<number>();
+  lastNumber: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onStartTime() {
+    this.globalInterval = setInterval(() => {
+      this.intervallFired.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    }, 1000);
   }
-
+  onStopTime() {
+    clearInterval(this.globalInterval);
+  }
 }
