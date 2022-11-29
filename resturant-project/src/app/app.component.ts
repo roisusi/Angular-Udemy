ae8123eb8748a2dc0708a8952d1a7093a4ea7898
roisusi <roisusi@gmail.com>
2022-11-29 10:48:51 +0200

    starting section 5

--- resturant-project/src/app/app.component.ts
+++ resturant-project/src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  menuSelected: string = '';
+  constructor() {}
+  toggleDisplay(selected: string) {
+    this.menuSelected = selected;
+  }
+}
--- resturant-project/src/app/header/header.component.ts
+++ resturant-project/src/app/header/header.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({ selector: 'app-header', templateUrl: './header.component.html' })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() onMenuToggle = new EventEmitter<string>();
+  onSelected(selected) {
+    this.onMenuToggle.emit(selected);
+  }
+}
