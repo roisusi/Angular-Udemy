42dc7501654a36839df5fc4977566a3c62dfcef9
Roi Susi <roisusi@gmail.com>
2023-02-16 15:47:47 +0200

    assignment forms reactive

--- forms-reactive-assignment/src/app/app.component.ts
+++ forms-reactive-assignment/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  assForm = new FormGroup({
+    "projectName": new FormControl(),
+    "email": new FormControl(),
+    "projectStatuus": new FormControl(),
+    });
 }
