b365913b3eb4b2977b236672e427a70245811166
Roi Susi <roisusi@gmail.com>
2023-02-16 17:49:19 +0200

    Prepare assignment complete

--- forms-reactive-assignment/src/app/app.component.ts
+++ forms-reactive-assignment/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {Project} from './project';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +10,46 @@ import {FormGroup, FormControl, Validators} from '@angular/forms';
 })
 export class AppComponent {
   projectStatusOptions = ['Stable', 'Critical', 'Finished'];
+  projectStatus  = ['Test'];
+  formProject: Project;
+  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5','Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5','Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5','Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+  containerHeight = '200px';
   assForm = new FormGroup({
-    'projectName': new FormControl(null, [Validators.required]),
-    'email': new FormControl(null, [Validators.required, Validators.email]),
-    'projectStatus': new FormControl(this.projectStatusOptions),
+    'projectName': new FormControl(null, [Validators.required, this.forbiddenName.bind(this)]),
+    'email': new FormControl(null, [Validators.required], this.forbiddenEmail.bind(this)),
+    'projectStatus': new FormControl(this.projectStatusOptions[1]),
   });
+  onSubmit() {
+    this.formProject = {
+      projectName: this.assForm.get(['projectName']).value,
+      email: this.assForm.get(['email']).value,
+      projectStatus: this.assForm.get(['projectStatus']).value,
+    };
+    console.log(this.formProject);
+  }
+  forbiddenName(control: FormControl): { [s: string]: boolean } {
+    if (this.projectStatus.indexOf(control.value) !== -1) {
+      return {
+        'nameIsForbidden': true
+      };
+    }
+    return null;
+  }
 
+  forbiddenEmail(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'roisusi@gmail.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+
+  update(event: Event) {
+    console.log((<HTMLSelectElement>event.target).value);
+  }
 }
---
+++ forms-reactive-assignment/src/app/project.ts
@@ -0,0 +1,5 @@
+export interface Project {
+  projectName: string;
+  email: string;
+  projectStatus: string;
+}
